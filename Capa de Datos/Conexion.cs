092417ef287bef894e88182c6a43ac6b2a5edf29
Carlos Morales Ibarra <cmorales@tostador.onmicrosoft.com>
2019-08-17 13:38:42 -0600

    Capa de datos y clase de conexion

---
+++ Capa de Datos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Capa_de_Datos
+{
+    public class Conexion
+    {
+
+
+    }
+}
---
+++ Capa de Datos/Parametros.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace Capa_de_Datos
+{
+    public class Parametros
+    {
+        //Parametros
+        public String Nombre { get; set; } //nombre de variable
+        public Object Valor { get; set; } //dato de la variable
+        public SqlDbType TipoDato { get; set; } //Tipo de dato de la variable
+        public Int32 Tamanno { get; set; } //Tamaño del tipo de dato de la variable
+        public ParameterDirection Direccion { get; set; } //Direccion de la variable ya sea de entrada o de salida
+        
+        //Constructores
+        //Entrada
+        public Parametros(string nombre, object valor)
+        {
+            Nombre = nombre;
+            Valor = valor;
+            Direccion = ParameterDirection.Input;
+        }
+
+        //Salida
+        public Parametros(string nombre, object valor, int tamanno)
+        {
+            Tamanno = tamanno;
+            Nombre = nombre;
+            Valor = valor;
+            Direccion = ParameterDirection.Output;
+        }
+
+        
+
+
+    }
+}
